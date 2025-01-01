9775618465929793e118377b9b77d974ef545be7
Paulo Ricardo <plricardo504@gmail.com>
2025-01-01 00:18:28 -0300

    feat: create dashboards dinamically based on current active channels

--- src/metrics.py
+++ src/metrics.py
@@ -2,6 +2,7 @@ import asyncio
 from datetime import datetime
 
 import psutil
+import requests
 from prometheus_client import Gauge, start_http_server
 
 from src.config import settings
@@ -26,20 +27,33 @@ class MetricsTracker:
         logger.info("Initializing metrics tracker")
 
         self._initialize_gauges()
+        self._initialize_grafana_dashboard()
 
     # Initialization methods ==================================================
 
     def _initialize_gauges(self):
         for streaming_manager in self.streaming_managers:
             self.cpu_gauges[streaming_manager.channel_name] = Gauge(
-                f"{streaming_manager.channel_name}_cpu_percent",
-                f"CPU usage of the channel {streaming_manager.channel_name}",
+                name=f"{streaming_manager.channel_name}_cpu_percent",
+                documentation=f"CPU usage percentage for {streaming_manager.channel_name}",
             )
 
             self.memory_gauges[streaming_manager.channel_name] = Gauge(
-                f"{streaming_manager.channel_name}_memory_usage",
-                f"Memory usage of the channel {streaming_manager.channel_name}",
+                name=f"{streaming_manager.channel_name}_memory_usage",
+                documentation=f"Memory usage in bytes for {streaming_manager.channel_name}",
             )
+
+    def _initialize_grafana_dashboard(self):
+        url = "http://localhost:3000/api/dashboards/db"
+        headers = {"Content-Type": "application/json"}
+
+        payload = self._build_dashboard_payload()
+        response = requests.post(url, json=payload, headers=headers)
+
+        if response.status_code == 200:
+            logger.info("Grafana dashboard created successfully")
+        else:
+            logger.error("Failed to create Grafana dashboard")
 
     # Public methods =========================================================
 
@@ -97,3 +111,52 @@ class MetricsTracker:
                 with open(settings.hls_output + "/" + streaming_manager.channel_name + "/metrics.log", "a") as log_file:
                     timestamp = datetime.now().strftime("%H:%M:%S")
                     log_file.write(f"[{timestamp}] Failed to collect metrics: {str(e)}\n")
+
+    def _build_dashboard_payload(self):
+        cpu_panels = []
+        memory_panels = []
+
+        for index, streaming_manager in enumerate(self.streaming_managers):
+            cpu_title = f"{streaming_manager.channel_name.capitalize()} CPU Usage"
+            cpu_expression = f"{streaming_manager.channel_name}_cpu_percent"
+
+            memory_title = f"{streaming_manager.channel_name.capitalize()} Memory Usage"
+            memory_expression = f"{streaming_manager.channel_name}_memory_usage"
+
+            cpu_panels.append(self._build_panel_payload(cpu_title, cpu_expression, x=0, y=index * 8))
+            memory_panels.append(self._build_panel_payload(memory_title, memory_expression, x=12, y=index * 8))
+
+        return {
+            "dashboard": {
+                "id": None,
+                "uid": "fe8mwxn1jury8d",
+                "title": "Video Stream Dashboard",
+                "tags": [],
+                "panels": cpu_panels + memory_panels,
+                "schemaVersion": 36,
+                "version": 0,
+            },
+            "folderId": 0,
+            "overwrite": True,
+        }
+
+    def _build_panel_payload(self, title, expression, x, y):
+        return {
+            "type": "timeseries",
+            "datasource": "Prometheus",
+            "title": title,
+            "targets": [
+                {
+                    "disableTextWrap": False,
+                    "editorMode": "builder",
+                    "expr": expression,
+                    "fullMetaSearch": False,
+                    "includeNullMetadata": True,
+                    "legendFormat": "__auto",
+                    "range": True,
+                    "refId": "A",
+                    "useBackend": False,
+                }
+            ],
+            "gridPos": {"x": x, "y": y, "h": 8, "w": 12},
+        }
